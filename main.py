6b534cfee2739c4465fc0941f0b44a5f4c70435c
Leonardo <leonardoalvesbezerra@gmail.com>
2025-05-17 00:33:39 -0300

    Add OCR files (developed on personal repo at https://github.com/leonardoabezerra/ocr_SideChef with 18 commits) + update main.py and requirements.txt

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from recipe.routes.recipe import router
+from recipe.routes.recipe import router as recipe_router
+from ocr import router as ocr_router
 
 app = FastAPI()
 
-app.include_router(router)
-
+app.include_router(recipe_router)
+app.include_router(ocr_router)
 
 @app.get("/health", tags=["Health Checks"])
 def read_root():
