5b798a067d2406eea25e346f22ec551ee807f34d
Yasmin Dayrell <yasminsduty2@gmail.com>
2025-05-18 16:20:58 -0300

    rota que busca uma receita com todos os ingredientes

--- recipe/routes/recipe.py
+++ recipe/routes/recipe.py
@@ -21,14 +21,46 @@ async def create_item(item: Item):
 #         for item in items
 #     ]
 
-@router.get("/recipes")
+@router.get("/oneIngredient")
 def get_recipes(ingrediente : str):
-                                            #como é uma str e não uma list usa regex
-        cursor = db["recipes"].find({"Ingredientes": {"$regex": fr"\b{ingrediente}\b", "$options": "i"}})
-        items = []
-        for item in cursor:
-            item["id"] = str(item["_id"])
-            item.pop("_id")
-            items.append(item)
-        return items
+                                #como é uma str e não uma list usa regex     #aceitar plural
+    print("procura de acordo com um item")                                               #case insensitive
+    query = db["recipes"].find({"Ingredientes": {"$regex": fr"\b{ingrediente}a*o*s*\b", "$options": "i"}})        
+    items = []
+    for item in query:
+        item["id"] = str(item["_id"])
+        item.pop("_id")
+        items.append(item)
+    return items
+
+
+#usar o método post, porque get não suportou entrada de dados mais complexos como listas
+@router.post("/allIngredients")
+def get_recipes(ingredients : list[str]):
+
+    ingredientsList = []
+    #garantir que não tem nenhum espaço em branco
+    for item in ingredients:
+        ingredientsList.append(item.strip())
+
+    #criar um lsita com as querys de cada ingrediente da lista
+    filters = []
+    for item in ingredientsList:
+        f = {"Ingredientes": {"$regex": fr"\b{item}a*o*s*\b", "$options": "i"}}
+        filters.append(f)
+
+    #buscar receitas com todos os ingredientes (operador "$and")
+    query = db["recipes"].find({
+        "$and": filters
+    })
+
+    items = []
+    for item in query:
+        item["id"] = str(item["_id"])
+        item.pop("_id")
+        items.append(item)
+
+    if not items:
+        raise HTTPException(status_code=404, detail="Não há receitas com todos esses ingredientes.")
+    return items
 
