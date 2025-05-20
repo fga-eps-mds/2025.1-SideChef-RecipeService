cefb3b28c8e384b055dd9500c74d37446f70c2ee
diogjunior100 <diogjunior10071@gmail.com>
2025-05-19 23:54:40 -0300

    fix: edit getRecipes router

--- recipe/routes/recipe.py
+++ recipe/routes/recipe.py
@@ -15,11 +15,28 @@ async def create_recipe(recipe: Recipe):
     if db is None:
         raise HTTPException(status_code=500, detail="Database connection error")
     recipes_collection = db["recipes"]
+
     if recipes_collection.find_one({"Nome": recipe.Nome}):
         raise HTTPException(status_code=400, detail="Recipe with this name already exists")
     
     recipes_collection.insert_one(recipe.model_dump())
     return {"message": "Recipe created successfully", "recipe": recipe}
+
+@router.get("/getRecipes")
+async def get_recipes(name: Optional[str] = Query(None, description="Optional name filter for recipes")):
+    if db is None:
+        raise HTTPException(status_code=500, detail="Database connection error")
+
+    recipes_collection = db["recipes"]
+
+    query = {"name": name} if name else {}
+
+    recipes = list(recipes_collection.find(query))
+
+    for recipe in recipes:
+        recipe["_id"] = str(recipe["_id"])
+
+    return recipes
 
 # @router.get("/items/")
 # async def list_items():
@@ -96,19 +113,3 @@ def get_recipes(ingredients : list[str]):
     })
 
     raise HTTPException(status_code=404, detail="Não há receitas com nenhum desses ingredientes.")
-
-@router.get("/getRecipes")
-async def get_recipes(name: Optional[str] = Query(None, description="Optional name filter for recipes")):
-    if db is None:
-        raise HTTPException(status_code=500, detail="Database connection error")
-
-    recipes_collection = db["recipes"]
-
-    query = {"name": name} if name else {}
-
-    recipes = list(recipes_collection.find(query))
-
-    for recipe in recipes:
-        recipe["_id"] = str(recipe["_id"])
-
-    return recipes
