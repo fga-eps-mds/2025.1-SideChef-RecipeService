4fbfa3225ee1fb61e5ce3b46eef135aecca00d9e
vinicius-jesus <vjbfvinicius@hotmail.com>
2025-06-04 13:39:33 -0300

    fix(test_recipe): removing  async definitions

--- tests/test_recipe.py
+++ tests/test_recipe.py
@@ -39,7 +39,7 @@ class MockData:
     
 @pytest.fixture
 def mock_mongo_collection():
-    collection = AsyncMock()
+    collection = MagicMock()
 
     collection._mock_data = [
         MockData.existing_recipe,
@@ -49,19 +49,32 @@ def mock_mongo_collection():
             difficulty="Fácil",
             ingredients=["leite", "morango"],
             preparation="Bata tudo no liquidificador"
-        )
+        ), MockData.make_recipe(
+            name="Bolo de Chocolate",
+            recipe_type="Doce",
+            difficulty="Fácil",
+            ingredients=["chocolate", "açúcar", "farinha"],
+            preparation="Bata tudo no liquidificador e leve ao forno por 30 minutos"
+        ), 
     ]
 
-    async def mock_find_one(query: Dict = None, **kwargs) -> Dict:
+    def mock_find_one(query: Dict = None, **kwargs) -> Dict:
         for doc in collection._mock_data:
             if query.get("Nome") == doc.get("Nome"):
                 return doc.copy()
         return None
+    
+    def mock_find(query: str = None, **kwargs):
+        if not query:
+            return collection._mock_data
+        return [doc for doc in collection._mock_data if query in doc.get("Ingredientes", [])]
 
-    # Testar se funciona
-    collection.find_one = AsyncMock(side_effect=mock_find_one)
-    # Testar se funciona
-    collection.insert_one = AsyncMock()
+
+    collection.find_one = MagicMock(side_effect=mock_find_one)
+
+    collection.insert_one = MagicMock()
+
+    collection.find = MagicMock(side_effect=mock_find)
 
     return collection
 
@@ -97,8 +110,8 @@ def test_create_recipe_success(test_client, mock_mongo_collection):
         "message": "Recipe created successfully", 
         "recipe": single_recipe}
     # Testa se função mockada foi chamada uma vez com o devido parâmetro
-    mock_mongo_collection.find_one.assert_awaited_once_with({"Nome": single_recipe["Nome"]})
-    mock_mongo_collection.insert_one.assert_awaited_once_with(single_recipe)
+    mock_mongo_collection.find_one.assert_called_once_with({"Nome": single_recipe["Nome"]})
+    mock_mongo_collection.insert_one.assert_called_once_with(single_recipe)
 
 def test_create_recipe_error_no_connection(test_client_no_db):
     
@@ -113,6 +126,20 @@ def test_create_recipe_error_already_created(test_client, mock_mongo_collection)
 
     assert response.status_code == 400
     assert response.json() == {"detail": "Recipe with this name already exists"}
-    mock_mongo_collection.find_one.assert_awaited_once_with({"Nome": existing_recipe["Nome"]})
-    mock_mongo_collection.insert_one.assert_not_awaited()
+    mock_mongo_collection.find_one.assert_called_once_with({"Nome": existing_recipe["Nome"]})
+    mock_mongo_collection.insert_one.assert_not_called()
  
+
+def test_filter_one_ingredient(test_client, mock_mongo_collection):
+    response = test_client.get("/recipe/oneIngredient", params={"ingrediente": "leite"})
+
+    assert response.status_code == 200
+    response = response.json()
+    recipes = response.get("recipes", [])
+
+    assert len(recipes) == 2
+    assert all("leite" in recipe["Ingredientes"] for recipe in recipes)
+
+    mock_mongo_collection.find.assert_called_once_with({
+        "Ingredientes": {"$regex": fr"\bleitea*o*s*\b", "$options": "i"}
+    })
