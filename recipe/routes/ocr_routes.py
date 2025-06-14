6654ce6e5e2466954a8d7e28ec3676fcd61d9dca
Leonardo Bezerra <leonardoalvesbezerra@gmail.com>
2025-06-14 13:20:05 -0300

    feat: update logic to handle multiple files

--- recipe/routes/ocr_routes.py
+++ recipe/routes/ocr_routes.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, UploadFile, File
+from typing import File
 import numpy as np
 import cv2
 import recipe.routes.utils.ocr_utils as ocr
@@ -13,45 +14,50 @@ router = APIRouter(
 
 # Endpoint to run OCR receiving target image as POST method
 @router.post("/run-ocr/")
-async def run_ocr(file: UploadFile = File(...)):
-  try:
-      # Read file
-    file_contents = await file.read()
+async def run_ocr(files: File[UploadFile] = File(...)):
+  for file in files:
 
-      # Convert to NumPy array then cv2 format
-    np_array = np.frombuffer(file_contents, np.uint8)
-    image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
-    
-    if image is None:
-      return {"error": "Image not found"}
+    try:
+      all_results = []
+        # Read file
+      file_contents = await file.read()
 
-    process_image = ocr.Enhance(image)  # Initialize class with target image
-    processed_image = process_image.execute()  # Run execute function
-
-      # Extract OCR output
-    extracted_text = ocr.run_ocr(processed_image)
-
-      # Send OCR's results to gemini and get response
-    initialize_gemini = ocr.Gemini(extracted_text)
-    product = initialize_gemini.execute()
-
-      # Check if reading failed
-    if product.strip() == "reading_failed":
-      processed_image = process_image.invert_image()  # Try alternative image processing  
+        # Convert to NumPy array then cv2 format
+      np_array = np.frombuffer(file_contents, np.uint8)
+      image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
       
-      extracted_text = ocr.run_ocr(processed_image)  # Re-run ocr
+      if image is None:
+        all_results.append({"filename": file.filename, "error": "Image not found"})
+        continue
 
-      initialize_gemini = ocr.Gemini(extracted_text)  # Initialize gemini again and send new OCR output
+      process_image = ocr.Enhance(image)  # Initialize class with target image
+      processed_image = process_image.execute()  # Run execute function
+
+        # Extract OCR output
+      extracted_text = ocr.run_ocr(processed_image)
+
+        # Send OCR's results to gemini and get response
+      initialize_gemini = ocr.Gemini(extracted_text)
       product = initialize_gemini.execute()
 
-    process_image.store_process_images()  # (for development only)
+        # Check if reading failed
+      if product.strip() == "reading_failed":
+        processed_image = process_image.invert_image()  # Try alternative image processing  
+        
+        extracted_text = ocr.run_ocr(processed_image)  # Re-run ocr
 
-    if product.strip() == "reading_failed":
-      return [f"reading_failed: {str(extracted_text.strip())}"]  
-    else: 
-      recipes = ocr.filter_recipes(product.strip())
-      return recipes
+        initialize_gemini = ocr.Gemini(extracted_text)  # Initialize gemini again and send new OCR output
+        product = initialize_gemini.execute()
+
+      process_image.store_process_images()  # (for development only)
+
+      if product.strip() == "reading_failed":
+        all_results.append({"error": f"reading_failed: {str(extracted_text.strip())}"})  # Note: find a way to deal with errors on filter_recipe or mobile side
+      else: 
+        all_results.append(product.strip())
+    
+    except Exception as err:
+      return {"error": f"File upload failed: {str(err)}"}
   
-  except Exception as err:
-    return {"error": f"File upload failed: {str(err)}"}
-  
+  recipes = ocr.filter_recipes(all_results)
+  return recipes
