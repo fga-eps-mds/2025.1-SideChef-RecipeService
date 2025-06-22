b694261b9ececedb7f5ad2d7dcd4676ce14c62d1
JoãoMarceloGuimarães <joaomarcelogcn@email.com>
2025-06-22 16:03:31 -0300

    feat: integrate image URLs and display in recipe list and details

--- recipe/models/recipe.py
+++ recipe/models/recipe.py
@@ -5,9 +5,14 @@ class Ingredient(BaseModel):
     quantidade: Optional[str] = ""
     ingrediente: str
 
+
+
+#Need to change the var's name to english here and in "SideChef-Mobile"!
 class Recipe(BaseModel):
     Nome: str
     Tipo: str
     Dificuldade: str
     Ingredientes: List[Ingredient]
     Preparo: str
+    image_url: Optional[str] = Field(None, description=" URL da imagem") 
+
